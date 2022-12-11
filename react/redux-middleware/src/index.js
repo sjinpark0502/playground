830b2821cf550b5c0c0c5acc262783097cd5b3c0
SuJin <sujin_park@day1company.co.kr>
2022-12-11 22:23:50 +0900

    middleware 적용 전 사전 셋팅

--- react/redux-middleware/src/index.js
+++ react/redux-middleware/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./modules";
+const store = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
