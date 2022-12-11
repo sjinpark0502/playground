bc9b96cbdf8fe58a35f3e7303983b34809f69fdf
SuJin <sujin_park@day1company.co.kr>
2022-12-11 20:33:44 +0900

    todo container로 redux 연결

--- react/redux-middleware/src/App.js
+++ react/redux-middleware/src/App.js
@@ -1,12 +1,13 @@
 import Todos from './components/Todos';
 import CounterContainer from './containers/CounterContainer';
+import TodoContainer from './containers/TodoContainer';
 
 function App() {
   return (
     <div>
       <CounterContainer />
       <hr />
-      <Todos />
+      <TodoContainer />
     </div>
   );
 }
