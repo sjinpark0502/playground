1c49a513b85911470f72d1d56d7a346ae952ad4e
SuJin <sujin_park@day1company.co.kr>
2022-12-11 19:43:45 +0900

    bind redux to component

--- react/redux-middleware/src/App.js
+++ react/redux-middleware/src/App.js
@@ -1,10 +1,11 @@
 import Counter from './components/Counter';
 import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
 
 function App() {
   return (
     <div>
-      <Counter number={0} />
+      <CounterContainer />
       <hr />
       <Todos />
     </div>
