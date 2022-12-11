1bdcdacdbd2255cf13e242bfbfe25f01f5482ad5
SuJin <sujin_park@day1company.co.kr>
2022-12-11 23:01:08 +0900

    setup thunk

--- react/redux-middleware/src/index.js
+++ react/redux-middleware/src/index.js
@@ -9,9 +9,10 @@ import { Provider } from "react-redux";
 import rootReducer from "./modules";
 // import loggerMiddleware from "./lib/loggerMiddleware";
 import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
